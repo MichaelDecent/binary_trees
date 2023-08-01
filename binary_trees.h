79af273113b3545b0d7f23969fe3285a0511ac32
MichaelDecent <michaeldecent2@gmail.com>
2023-08-01 11:27:09 +0000

    update on Binary Tree Project

--- binary_trees.h
+++ binary_trees.h
@@ -26,7 +26,7 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 
 /*AVL Tree*/
-typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
 
 /*Max Binary*/
 typedef struct binary_tree_s heap_t;
