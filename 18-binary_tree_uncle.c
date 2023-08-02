f3debdd9de42593c2571f9a563a28775e9b08a24
MichaelDecent <michaeldecent2@gmail.com>
2023-08-02 01:49:05 +0000

    added Task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -15,13 +15,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	left_uncle = node->parent->parent->left;
 	right_uncle = node->parent->parent->right;
 
-	if (node == node->parent->left)
-	{
-		if (left_uncle == NULL)
-			return (NULL);
+	if (node == node->parent->left && left_uncle != NULL)
 		return (left_uncle);
-	}
-	else if (node == node->parent->right && right_uncle == NULL)
-		return (NULL);
-	return (right_uncle);
+	else if (node == node->parent->right && right_uncle != NULL)
+		return (right_uncle);
+	return (NULL);
 }
